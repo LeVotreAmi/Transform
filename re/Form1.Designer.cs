e4ba62186bcd10a766fae2d90dc01329b5a92f74
LeVotreAmi <levotreami@gmail.com>
2019-07-02 14:49:11 +0300

    This program transform exel file into N-amount 'letters'

---
+++ re/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace re
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Таблица с данными (.xlsx)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(166, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Шаблон письма (.docx)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Silver;
+            this.button3.Location = new System.Drawing.Point(12, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(302, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Сгенерировать";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(12, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(302, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Папка для сохранения";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 150);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(302, 28);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(128, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Complete!";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(327, 186);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Transform";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ re/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel =  Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace re
+{
+    public partial class Form1 : Form
+    {
+        //Пути к файлам
+        static string pathTemplate = "";
+        static string pathTable = "";
+        static string pathSave = "";
+
+        //Свойства кнопки "Сгенерировать"
+        private void Properties(Button button3)
+        {
+            button3.Enabled = true;
+            button3.BackColor = Color.FromArgb(144, 238, 144);
+            button3.ForeColor = Color.FromArgb(0, 0, 0);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Получить путь до файла эксель
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Файлы xlsx|*.xlsx";
+            ofd.ShowDialog();
+
+            pathTable = ofd.FileName;
+            //Если все пути получены: включаем кнопку "Сгенерировать"
+            if (pathTable != "" && pathTemplate != "" && pathSave != "")
+            {
+                Properties(button3);
+            }
+        }
+
+        //Получить путь до файла ворд
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Файлы docx|*.docx|Файлы doc|*.doc";
+            ofd.ShowDialog();
+
+            pathTemplate = ofd.FileName;
+            if (pathTable != "" && pathTemplate != "" && pathSave != "")
+            {
+                Properties(button3);
+            }
+        }
+
+        //Получить директорию для сохранения писем
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+
+            pathSave = fbd.SelectedPath;
+            if (pathTable != "" && pathTemplate != "" && pathSave != "")
+            {
+                Properties(button3);
+            }
+        }
+
+        //Кнопка "Сгенерировать"
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            var WordApp = new Word.Application(); 
+            WordApp.Visible = false;
+
+            //Открыть ворд && открыть эксель, получить 1 лист
+            var WordDocument = WordApp.Documents.Open(@"" + pathTemplate);
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@"" + pathTable);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+
+            //Получить кол-во заполненных ячеек эксель
+            int nInLastRow = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious,
+                false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
+            int nInLastCol = ObjWorkSheet.Cells.Find("*", System.Reflection.Missing.Value,
+                System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                Excel.XlSearchOrder.xlByColumns, Excel.XlSearchDirection.xlPrevious,
+                false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Column;
+
+            //Св-ва програссбара и лейбла
+            progressBar1.Visible = true;
+            label1.Visible = false;
+            label1.Text = "";
+            progressBar1.Value = 0;
+            progressBar1.Step = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = nInLastRow;
+
+            string[,] list = new string[nInLastRow, nInLastCol]; //Равен по размеру листу
+
+            //Данные с листа в массив
+            for (int i = 0; i < nInLastRow; i++)
+            {
+                for (int j = 0; j < nInLastCol; j++)
+                {
+                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString();
+
+                }
+            }
+
+            //Данные из массива в ворд
+            try
+            {
+                for (int i = 0; i < nInLastRow; i++)
+                {
+                    for (int j = 0; j < nInLastCol; j++)
+                    {
+
+                        string tmp = list[i, j]; //Значение текущей ячейки в переменную
+                        ReplaceWordStub("{" + j + "}", tmp, WordDocument); //Меняем метку в шаблоне ворд на значение
+
+                        //Сохранить, когда будет обрабатываться последняя ячейка в строке
+                        if (j == nInLastCol-1)
+                        {
+                            WordDocument.SaveAs(@"" + pathSave + "\\" + list[i, j] + ".docx");
+                            //Закрыть, иначе будет множество экземпляров ворд в фоне, открыть шаблон снова для следующей строки
+                            WordDocument.Close(false, Type.Missing, Type.Missing);
+                            WordDocument = WordApp.Documents.Open(@"" + pathTemplate);
+                        }
+
+                    }
+                    
+                    //Показать прогресс
+                    progressBar1.Value += 1;
+
+                    if (progressBar1.Value == nInLastRow)
+                    {
+                        progressBar1.Visible = false;
+                        label1.Visible = true;
+                        label1.Text = "Complete!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.ToString();
+            }
+            finally
+            {
+                //Закрыть после завершения работы
+                WordDocument.Close(false, Type.Missing, Type.Missing);
+                WordApp.Quit();
+                ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+                ObjWorkExcel.Quit();
+            }
+        }
+
+        //Метод, который меняет метку и данные из ячейки местами
+        static void ReplaceWordStub(string StubToReplace, string Text, Word.Document WordDocument)
+        {
+
+            var Range = WordDocument.Content;
+            Range.Find.Execute(FindText: StubToReplace, ReplaceWith: Text);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GC.Collect(); // Убрать за собой
+        }
+    }
+}
